94c480f30ec8c3b1c15ba0686292888626b359b3
Paulo Souza <paulos@portasabertas.org.br>
2025-08-20 18:54:13 -0300

    teste

--- app/src/app.service.ts
+++ app/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Meu nome é Paulo!!!!!!';
+    return 'Hello World!';
   }
 }
